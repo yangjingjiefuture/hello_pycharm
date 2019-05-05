0f77cd4c8235febd7860cc84512b68defcd756b7
yangjingjiefuture <yangjjfuture@163.com>
2019-05-05 20:36:28 +0800

    11111

--- test_logout.py
+++ test_logout.py
@@ -1,6 +1,9 @@
 class Testlogout:
     def testlogout01(self):
         print("testlogout01")
-        
+
     def testlogout02(self):
+        print("testlogout02")
+
+    def testlogout04(self):
         print("testlogout02")
