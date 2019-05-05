93ac1a7c7cbfe6aa154a2b08dfafe78d43c10809
yangjingjiefuture <yangjjfuture@163.com>
2019-05-05 20:52:25 +0800

    修改logout

--- test_logout.py
+++ test_logout.py
@@ -5,5 +5,8 @@ class Testlogout:
     def testlogout02(self):
         print("testlogout02")
 
+    def testlogout03(self):
+        print("testlogout03")
+
     def testlogout04(self):
-        print("testlogout02")
+        print("testlogout04")
